c3ff3cda1ef954bcd8e5b35b42bdb0d3952f67b0
HibrahimBesili <hibesili533@icloud.com>
2021-11-29 23:34:50 +0300

    Duzeltme

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+let name = prompt("Adınız Nedir");
+
+document.getElementById("myName").innerHTML = name;
+
+let span = document.getElementById("myClock");
+
+function time() {
+    let d = new Date();
+    let s = d.getSeconds();
+    let m = d.getMinutes();
+    let h = d.getHours();
+    span.textContent = 
+      ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
+  }
+  
+  setInterval(time, 1000);
